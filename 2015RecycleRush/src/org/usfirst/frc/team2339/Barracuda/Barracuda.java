a6c45ba80640c55e764cc42ff59e55eba46191df
Jamestristan <tristanfaass@yahoo.com>
2015-02-05 18:38:32 -0800

    Removed Compressor & Updated Talon List- WIP

--- 2015RecycleRush/src/org/usfirst/frc/team2339/Barracuda/Barracuda.java
+++ 2015RecycleRush/src/org/usfirst/frc/team2339/Barracuda/Barracuda.java
@@ -1,12 +1,11 @@
 
-package org.usfirst.frc.team2339.Template;
+package org.usfirst.frc.team2339.Barracuda;
 
 import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Encoder;
-import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.PIDController;
 import edu.wpi.first.wpilibj.Preferences;
@@ -32,12 +31,7 @@ public class Barracuda extends IterativeRobot {
      * CLASS VARIABLES
     /**********************/
     
-    /*
-     * Smart dashboard preferences
-     */
-    private final Preferences preferences = Preferences.getInstance();
-    private int redLow;
-    
+
 	   /*
      * Initialize joystck variables.
      * The (1) and (2) refer hardward channels 1 and 2 on the robot
@@ -47,14 +41,8 @@ public class Barracuda extends IterativeRobot {
     private final Joystick driveStickRight = new Joystick(1);
     // Third joystick for shooter and lift control
     private final Joystick operatorStick = new Joystick(2);
-    private final DriverStation driverStation = DriverStation.getInstance();
-    // Class to interact with driver station
-    //private final DriverStationEnhancedIO driverStation = DriverStation.getInstance().getEnhancedIO();
-    
-    /*
-     * Initialize motor controllers.
-     */
-    private final Talon m_frontLeft = new Talon(0);
+
+    private final Talon m_frontLeft = new Talon(8);
     private final Talon m_frontRight= new Talon(1);
     private final Talon shooterMotorA = new Talon(2);
     private final Talon shooterMotorB = new Talon(3);
@@ -67,14 +55,7 @@ public class Barracuda extends IterativeRobot {
       private final RobotDrive robotDrive = 
               new RobotDrive(m_frontLeft, m_frontRight);
       
-      /*
-       * Initialize super shifter
-       */
-
-      private final int clawButton = 1;
-      private final Joystick clawJoystick = operatorStick;
-      private boolean isClawClosed = false;
-      private boolean wasClawButtonJustPushed = false;
+   
       /*
        * This method sets shooter winch motors
        * 
@@ -87,18 +68,6 @@ public class Barracuda extends IterativeRobot {
       }
       
        
-    /*
-     * Initialize compressor
-     */
-      
-    private final int compressorRelayChannel = 1;
-    private final Compressor compressor = new Compressor(compressorRelayChannel);
-    
-    
-    /*
-     * Initialize relay to control cooling fan
-     */
-    private final Relay fan = new Relay(2);
 
     /*
      * Initialize shooter motor data
@@ -137,7 +106,6 @@ public class Barracuda extends IterativeRobot {
         robotStartTime = System.currentTimeMillis();
         System.out.println("Robot init time: " + robotStartTime);
         
-        redLow = preferences.getInt("Red low", 200);
         //robotDrive.setInvertedMotor(RobotDrive.MotorType.kFrontLeft, true);
         //robotDrive.setInvertedMotor(RobotDrive.MotorType.kFrontRight, true);
         System.out.println("Before init: " + System.currentTimeMillis());
@@ -145,59 +113,21 @@ public class Barracuda extends IterativeRobot {
         System.out.println("After init: " + System.currentTimeMillis());
     }
     
-    private void setShootSolenoid(boolean b) {
-		// TODO Auto-generated method stub
-		
-	}
 
 	public void autonomousPeriodicInit() {
     	   startTime = System.currentTimeMillis();
            System.out.println("Autonomous init time: " + startTime);
-           // Start compressor
-           compressor.start();
-           // Turn on cooling fan at beginning of autonomous
-           fan.set(Relay.Value.kForward);
-           // Initialize drive encoder
-
-           haveShot = false;
-           haveImage = false;
+ 
        }
     public void autonomousPeriodic() {
-        long elapsed = System.currentTimeMillis() - startTime;
-        SmartDashboard.putNumber("Autonomous Elapsed time ", elapsed/1000.0);
-        double shooterSlider = 5.0;
-        double driveTimeSlider = 2.0;
-        double driveSpeedSlider = 5.0;
-        SmartDashboard.putNumber("Autonomous Shooter Slider ", shooterSlider);
-        SmartDashboard.putBoolean("Have shot ", haveShot);
-        
-
-        final long delayTime = 200;
-        // Start bot about 3 ft behind line.
-        final long driveTime = delayTime + 1500; 
-        final long drivePauseTime = driveTime + 1000;
-  
+     
         /*
          * This method is called at the beginning of operator control
          */
     }
      public void teleopInit() {
-            // Start compressor
-            compressor.start();
-            // Turn on cooling fan at beginning of teleop
-            fan.set(Relay.Value.kForward);
-            // Shift to high for teleop
-            isClawClosed = false;
+       
             
-            String operatorButtons = "";
-            for (int iiButton = 0; iiButton < 10; iiButton++) {
-                if (iiButton > 0) {
-                    operatorButtons += " ";
-                }
-                operatorButtons += (iiButton+1) + ":" + operatorStick.getRawButton(iiButton+1);
-            }
-            SmartDashboard.putString("Operator Buttons ", operatorButtons);
-
         }
 
  
@@ -207,12 +137,7 @@ public class Barracuda extends IterativeRobot {
      */
 
     public void teleopPeriodic() {
-    	
-          boolean pressure = compressor.getPressureSwitchValue();
-          /*
-           * Keep fan running
-           */
-          fan.set(Relay.Value.kForward);
+   
           
           /*
            * Get drive data from joystick
@@ -257,8 +182,7 @@ public class Barracuda extends IterativeRobot {
     public void testPeriodic() {
         long time = System.currentTimeMillis();
         SmartDashboard.putNumber("Test Mode Running Time ", time);
-        long elapsed = System.currentTimeMillis() - startTime;
-    	compressor.start();
+       // long elapsed = System.currentTimeMillis() - startTime;
 
     }
     
