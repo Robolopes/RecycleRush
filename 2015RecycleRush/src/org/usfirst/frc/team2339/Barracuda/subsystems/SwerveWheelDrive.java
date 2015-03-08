c94383f2cbea0898e285cdaba8c608387242da38
Eric Miller <eric.miller@spirent.com>
2015-03-08 16:47:44 -0700

    Implement swerve wheel drive.

--- 2015RecycleRush/src/org/usfirst/frc/team2339/Barracuda/subsystems/SwerveWheelDrive.java
+++ 2015RecycleRush/src/org/usfirst/frc/team2339/Barracuda/subsystems/SwerveWheelDrive.java
@@ -2,61 +2,93 @@ package org.usfirst.frc.team2339.Barracuda.subsystems;
 
 import edu.wpi.first.wpilibj.MotorSafety;
 import edu.wpi.first.wpilibj.MotorSafetyHelper;
+import edu.wpi.first.wpilibj.PIDController;
 import edu.wpi.first.wpilibj.SpeedController;
 
 /**
  * Class to provide swerve drive control to one wheel.
  * Wheel must have independent drive motor and steering motor.
- * A steering encoder provides feedback on wheel angle.  
+ * A steering encoder provides feedback on wheel angle.
+ *   
  * @author emiller
  *
  */
 public class SwerveWheelDrive implements MotorSafety {
 	
-    protected MotorSafetyHelper m_safetyHelper;
+    protected MotorSafetyHelper safetyHelper;
     public static final double kDefaultExpirationTime = MotorSafety.DEFAULT_SAFETY_EXPIRATION;
-
-    protected SpeedController m_driveController;
     
-    SwerveWheelDrive() {
+    protected int wheelNumber;
+    protected SpeedController driveController;
+    protected PIDController steeringController;
+    
+    SwerveWheelDrive(
+    		int wheelNumber, 
+    		SpeedController driveController, 
+    		PIDController steeringController) {
+        this.wheelNumber = wheelNumber;
+        this.driveController = driveController;
+        this.steeringController = steeringController;
     	setupMotorSafety();
     }
 
+    public void setWheelSpeed(double speed) {
+        driveController.set(speed);
+    }
+    
+    public void setSteeringAngle(double angle) {
+        steeringController.setSetpoint(angle);
+    }
+    
+    public void resetSteering() {
+    	boolean isEnabled = steeringController.isEnable();
+    	steeringController.reset();
+    	steeringEnable(isEnabled);
+    }
+    
+    public void steeringEnable(boolean enable) {
+    	if (enable) {
+    		steeringController.enable();
+    	} else {
+    		steeringController.disable();
+    	}
+    }
+    
     private void setupMotorSafety() {
-        m_safetyHelper = new MotorSafetyHelper(this);
+        safetyHelper = new MotorSafetyHelper(this);
         this.setExpiration(kDefaultExpirationTime);
         this.setSafetyEnabled(true);
     }
 
 	@Override
 	public void setExpiration(double timeout) {
-        m_safetyHelper.setExpiration(timeout);
+        safetyHelper.setExpiration(timeout);
 	}
 
 	@Override
 	public double getExpiration() {
-        return m_safetyHelper.getExpiration();
+        return safetyHelper.getExpiration();
 	}
 
 	@Override
 	public boolean isAlive() {
-        return m_safetyHelper.isAlive();
+        return safetyHelper.isAlive();
 	}
 
 	@Override
 	public void stopMotor() {
-		m_driveController.set(0.0);
-        if (m_safetyHelper != null) m_safetyHelper.feed();
+		driveController.set(0.0);
+        if (safetyHelper != null) safetyHelper.feed();
 	}
 
 	@Override
 	public void setSafetyEnabled(boolean enabled) {
-        m_safetyHelper.setSafetyEnabled(enabled);
+        safetyHelper.setSafetyEnabled(enabled);
 	}
 
 	@Override
 	public boolean isSafetyEnabled() {
-        return m_safetyHelper.isSafetyEnabled();
+        return safetyHelper.isSafetyEnabled();
 	}
 
 	@Override
