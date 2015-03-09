51f45b43a16fdca28a7c1abcab7f04d9d4f18f8a
Eric Miller <eric.miller@spirent.com>
2015-03-08 17:07:25 -0700

    Add swerve steering encoder class.

--- 2015RecycleRush/src/org/usfirst/frc/team2339/Barracuda/components/SwerveSteeringPidController.java
+++ 2015RecycleRush/src/org/usfirst/frc/team2339/Barracuda/components/SwerveSteeringPidController.java
@@ -1,6 +1,5 @@
 package org.usfirst.frc.team2339.Barracuda.components;
 
-import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.PIDController;
 import edu.wpi.first.wpilibj.SpeedController;
 
@@ -13,7 +12,7 @@ import edu.wpi.first.wpilibj.SpeedController;
 public class SwerveSteeringPidController extends PIDController {
 
 	public SwerveSteeringPidController(double Kp, double Ki, double Kd,
-			Encoder steeringEncoder, SpeedController steeringController) {
+			SwerveSteeringEncoder steeringEncoder, SpeedController steeringController) {
 		super(Kp, Ki, Kd, steeringEncoder, steeringController);
         this.setInputRange(-180, 180);
         this.setContinuous(true);
