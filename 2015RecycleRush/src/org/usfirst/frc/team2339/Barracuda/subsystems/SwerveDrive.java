53ca34332e7c5c4c77b5328b1a1419a981ffde77
Robolopes Development Team <robolopesdev@gmail.com>
2015-03-28 10:10:13 -0700

    Raise deadband value to 0.3. Disable deadband setting.

--- 2015RecycleRush/src/org/usfirst/frc/team2339/Barracuda/subsystems/SwerveDrive.java
+++ 2015RecycleRush/src/org/usfirst/frc/team2339/Barracuda/subsystems/SwerveDrive.java
@@ -160,9 +160,11 @@ public class SwerveDrive extends Subsystem {
 	 * Set speed and angle values when joystick in dead band
 	 */
 	public void setDeadBandValues() {
+		/*
     	for (int iiWheel = 0; iiWheel < wheels.length; iiWheel++) {
         	wheels[iiWheel].setWheel(new VelocityPolar(0, 0));
     	}
+    	*/
 	}
 	
     /**
