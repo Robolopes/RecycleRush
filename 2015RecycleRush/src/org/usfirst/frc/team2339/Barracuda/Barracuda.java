04a2e789ea23226a540f335d3d330665b5021846
RobolopesDev <RobolopesDev@Tahiti>
2015-03-28 09:43:06 -0700

    Turn off auto cancel in teleop init

--- 2015RecycleRush/src/org/usfirst/frc/team2339/Barracuda/Barracuda.java
+++ 2015RecycleRush/src/org/usfirst/frc/team2339/Barracuda/Barracuda.java
@@ -92,7 +92,7 @@ public class Barracuda extends IterativeRobot {
         // teleop starts running. If you want the autonomous to 
         // continue until interrupted by another command, remove
         // this line or comment it out.
-        if (autonomousCommand != null) autonomousCommand.cancel();
+        //if (autonomousCommand != null) autonomousCommand.cancel();
         
     	RobotMap.Analog.GYRO.reset();
     	RobotMap.Subsystem.robotDrive.resetSteering();
