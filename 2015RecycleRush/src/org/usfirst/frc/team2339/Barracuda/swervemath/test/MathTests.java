e49f91dc4d2c3c49b3707930052ba79ffd6108ec
EricGregMiller <EricGregMiller@gmail.com>
2015-03-28 21:07:30 -0700

    Add unit tests.

---
+++ 2015RecycleRush/src/org/usfirst/frc/team2339/Barracuda/swervemath/test/MathTests.java
@@ -0,0 +1,89 @@
+package org.usfirst.frc.team2339.Barracuda.swervemath.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.usfirst.frc.team2339.Barracuda.swervemath.SwerveWheel;
+import org.usfirst.frc.team2339.Barracuda.swervemath.SwerveWheel.RobotMotion;
+import org.usfirst.frc.team2339.Barracuda.swervemath.SwerveWheel.VelocityPolar;
+import org.usfirst.frc.team2339.Barracuda.swervemath.WorkingPalmdale;
+import org.usfirst.frc.team2339.Barracuda.swervemath.WorkingPalmdale.WheelData;
+
+public class MathTests {
+
+	@Test
+	public void testPalmdaleRotate() {
+		/*
+		 * 45 degree angle case
+		 */
+		WorkingPalmdale.WHEEL_BASE_LENGTH = 25;
+		WorkingPalmdale.WHEEL_BASE_WIDTH = 25;
+		WheelData rawWheelData = WorkingPalmdale.calculateRawWheelData(0.0, 0.0, 1.0);
+		WheelData rawWheelDataGen = WorkingPalmdale.calculateRawWheelDataGeneral(0.0, 0.0, 1.0, 0.0, 0.0);
+		for (int ii = 0; ii < WorkingPalmdale.kMaxNumberOfMotors; ii++) {
+			System.out.println("Wheel " + ii + ": speed " + rawWheelData.wheelSpeeds[ii] +
+					", angle " + rawWheelData.wheelAngles[ii]);
+			System.out.println("Wheel " + ii + ": Gen speed " + rawWheelDataGen.wheelSpeeds[ii] +
+					", angle " + rawWheelDataGen.wheelAngles[ii]);
+			assertEquals(rawWheelDataGen.wheelSpeeds[ii], rawWheelData.wheelSpeeds[ii], 0.01);
+			assertEquals(rawWheelDataGen.wheelAngles[ii], rawWheelData.wheelAngles[ii], 0.01);
+		}
+
+		rawWheelData = WorkingPalmdale.calculateRawWheelData(1.0, 0.0, 1.0);
+		rawWheelDataGen = WorkingPalmdale.calculateRawWheelDataGeneral(1.0, 0.0, 1.0, 0.0, 0.0);
+		for (int ii = 0; ii < WorkingPalmdale.kMaxNumberOfMotors; ii++) {
+			System.out.println("Wheel " + ii + ": speed " + rawWheelData.wheelSpeeds[ii] +
+					", angle " + rawWheelData.wheelAngles[ii]);
+			System.out.println("Wheel " + ii + ": Gen speed " + rawWheelDataGen.wheelSpeeds[ii] +
+					", angle " + rawWheelDataGen.wheelAngles[ii]);
+			assertEquals(rawWheelDataGen.wheelSpeeds[ii], rawWheelData.wheelSpeeds[ii], 0.01);
+			assertEquals(rawWheelDataGen.wheelAngles[ii], rawWheelData.wheelAngles[ii], 0.01);
+		}
+		
+		rawWheelData = WorkingPalmdale.calculateRawWheelData(0.0, 1.0, 1.0);
+		rawWheelDataGen = WorkingPalmdale.calculateRawWheelDataGeneral(0.0, 1.0, 1.0, 0.0, 0.0);
+		for (int ii = 0; ii < WorkingPalmdale.kMaxNumberOfMotors; ii++) {
+			System.out.println("Wheel " + ii + ": speed " + rawWheelData.wheelSpeeds[ii] +
+					", angle " + rawWheelData.wheelAngles[ii]);
+			System.out.println("Wheel " + ii + ": Gen speed " + rawWheelDataGen.wheelSpeeds[ii] +
+					", angle " + rawWheelDataGen.wheelAngles[ii]);
+			assertEquals(rawWheelDataGen.wheelSpeeds[ii], rawWheelData.wheelSpeeds[ii], 0.01);
+			assertEquals(rawWheelDataGen.wheelAngles[ii], rawWheelData.wheelAngles[ii], 0.01);
+		}
+		
+		/*
+		 * Robolope dimensions
+		 */
+		WorkingPalmdale.WHEEL_BASE_LENGTH = 28.5;
+		WorkingPalmdale.WHEEL_BASE_WIDTH = 25;
+		rawWheelData = WorkingPalmdale.calculateRawWheelData(0.0, 0.0, 1.0);
+		rawWheelDataGen = WorkingPalmdale.calculateRawWheelDataGeneral(0.0, 0.0, 1.0, 0.0, 0.0);
+		for (int ii = 0; ii < WorkingPalmdale.kMaxNumberOfMotors; ii++) {
+			System.out.println("Wheel " + ii + ": speed " + rawWheelData.wheelSpeeds[ii] +
+					", angle " + rawWheelData.wheelAngles[ii]);
+			System.out.println("Wheel " + ii + ": Gen speed " + rawWheelDataGen.wheelSpeeds[ii] +
+					", angle " + rawWheelDataGen.wheelAngles[ii]);
+			assertEquals(rawWheelDataGen.wheelSpeeds[ii], rawWheelData.wheelSpeeds[ii], 0.01);
+			assertEquals(rawWheelDataGen.wheelAngles[ii], rawWheelData.wheelAngles[ii], 0.01);
+		}
+
+	}
+
+	@Test
+	public void testNewRotate() {
+		VelocityPolar[] rawWheelData = SwerveWheel.calculateRectangularWheelVelocities(25, 25, 
+				new RobotMotion(0.0, 0.0, 1.0));
+		for (int ii = 0; ii < WorkingPalmdale.kMaxNumberOfMotors; ii++) {
+			System.out.println("Wheel " + ii + ": speed " + rawWheelData[ii].speed +
+					", angle " + rawWheelData[ii].angle);
+		}
+
+		rawWheelData = SwerveWheel.calculateRectangularWheelVelocities(25, 25, 
+				new RobotMotion(0.0, 0.0, -1.0));
+		for (int ii = 0; ii < WorkingPalmdale.kMaxNumberOfMotors; ii++) {
+			System.out.println("Wheel " + ii + ": speed " + rawWheelData[ii].speed +
+					", angle " + rawWheelData[ii].angle);
+		}
+
+	}
+}
