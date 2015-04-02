8f63ce4e0c0286780aa7ec39175b8e9b2b9f488e
Robolopes Development Team <robolopesdev@gmail.com>
2015-04-02 09:47:58 -0700

    Create auto settings dash object.

---
+++ 2015RecycleRush/src/org/usfirst/frc/team2339/Barracuda/smartdashboard/AutoSettings.java
@@ -0,0 +1,45 @@
+/**
+ * 
+ */
+package org.usfirst.frc.team2339.Barracuda.smartdashboard;
+
+import edu.wpi.first.wpilibj.Sendable;
+import edu.wpi.first.wpilibj.tables.ITable;
+
+/**
+ * @author RobolopesDev
+ *
+ */
+public class AutoSettings implements Sendable {
+	
+    private ITable table;
+    private double forward;
+
+	/* (non-Javadoc)
+	 * @see edu.wpi.first.wpilibj.Sendable#initTable(edu.wpi.first.wpilibj.tables.ITable)
+	 */
+	@Override
+	public void initTable(ITable subtable) {
+        this.table = subtable;
+        if (table != null) {
+            table.putValue("forward", forward);
+        }
+	}
+
+	/* (non-Javadoc)
+	 * @see edu.wpi.first.wpilibj.Sendable#getTable()
+	 */
+	@Override
+	public ITable getTable() {
+		return table;
+	}
+
+	/* (non-Javadoc)
+	 * @see edu.wpi.first.wpilibj.Sendable#getSmartDashboardType()
+	 */
+	@Override
+	public String getSmartDashboardType() {
+		return "Robolopes Autonomous Settings";
+	}
+
+}
