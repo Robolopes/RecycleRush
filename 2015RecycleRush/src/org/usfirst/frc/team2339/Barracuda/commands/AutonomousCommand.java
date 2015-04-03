0f36b9cd892da3cabd72c8e812eb5160e9762354
Robolopes Development Team <robolopesdev@gmail.com>
2015-04-03 10:31:33 -0700

    Change auto backup to 1.5 seconds.

--- 2015RecycleRush/src/org/usfirst/frc/team2339/Barracuda/commands/AutonomousCommand.java
+++ 2015RecycleRush/src/org/usfirst/frc/team2339/Barracuda/commands/AutonomousCommand.java
@@ -60,6 +60,6 @@ public class AutonomousCommand extends CommandGroup {
     	*/
     	//addSequential(new TimedDrive("Push RC to wall", robotDrive, 1.25, 0.5, 0.0)); 
     	addSequential(new TimedLift("Pick up RC", lift, 1, 0.5)); 
-    	addSequential(new TimedDrive("Backup to auto zone", robotDrive, 1.25, -0.5, 0.0)); 
+    	addSequential(new TimedDrive("Backup to auto zone", robotDrive, 1.5, -0.5, 0.0)); 
     }
 }
