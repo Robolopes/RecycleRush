793ad54fbea79340b85bbd6f0041a7a54a8680fb
Robolopes Development Team <robolopesdev@gmail.com>
2015-04-03 13:35:55 -0700

    Auto strafe left.

--- 2015RecycleRush/src/org/usfirst/frc/team2339/Barracuda/commands/AutonomousCommand.java
+++ 2015RecycleRush/src/org/usfirst/frc/team2339/Barracuda/commands/AutonomousCommand.java
@@ -60,6 +60,7 @@ public class AutonomousCommand extends CommandGroup {
     	*/
     	//addSequential(new TimedDrive("Push RC to wall", robotDrive, 1.25, 0.5, 0.0)); 
     	addSequential(new TimedLift("Pick up RC", lift, 1, 0.5)); 
-    	addSequential(new TimedDrive("Backup to auto zone", robotDrive, 1.5, -0.5, 0.0)); 
+    	//addSequential(new TimedDrive("Backup to auto zone", robotDrive, 1.5, -0.5, 0.0)); 
+    	addSequential(new TimedDrive("Strafe left to auto zone", robotDrive, 1.5, 0.5, 90.0)); 
     }
 }
